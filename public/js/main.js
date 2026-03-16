c31d37404783cdc5824c8600c1187d9bae9e39f5
angelinara <106839384+angelinara@users.noreply.github.com>
2026-03-16 14:35:39 +0000

    updated contact page with location details

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,19 @@ function resetScrollOnRefresh() {
 }
 
 resetScrollOnRefresh();
+
+// prompt to specify location on contact page
+function showOtherLocation() {
+  const select = document.getElementById("location");
+  const otherBox = document.getElementById("otherLocationBox");
+  const otherInput = document.getElementById("otherLocation");
+
+  if (select.value === "Other") {
+    otherBox.style.display = "block";
+    otherInput.required = true;
+  } else {
+    otherBox.style.display = "none";
+    otherInput.required = false;
+    otherInput.value = "";
+  }
+}
